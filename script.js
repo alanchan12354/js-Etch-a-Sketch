5e27f93963412bed75cd48b2483afe866842c8cd
CheungHong CHAN <67678585+alanchan12354@users.noreply.github.com>
2025-11-09 22:23:54 +0800

    Refactor JavaScript code structure and add reset button functionality

--- script.js
+++ script.js
@@ -2,16 +2,36 @@ let x = 16;
 let y = 16;
 
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".reset");
 
-for (let i = 0; i < x; i++) {
-    let row = document.createElement("div");
-    row.classList.add(`row`);
-    row.id = `row${i}`;
-    container.appendChild(row);
-    for (let j = 0; j < y; j++) {
-        let grid = document.createElement("div");
-        grid.classList.add(`grid`);
-        grid.id = `grid${i}${j}`;
-        row.appendChild(grid);
+const createCanva = (x, y) => {
+    for (let i = 0; i < x; i++) {
+        let row = document.createElement("div");
+        row.classList.add(`row`);
+        row.id = `row${i}`;
+        container.appendChild(row);
+        for (let j = 0; j < y; j++) {
+            let grid = document.createElement("div");
+            grid.classList.add(`grid`);
+            grid.id = `grid${i}${j}`;
+            row.appendChild(grid);
+        }
     }
 }
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid")) {
+        e.target.style.backgroundColor = "black";
+    }
+})
+
+resetBtn.addEventListener("click", (e) => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    x = parseInt(prompt("Enter x:"));
+    y = parseInt(prompt("Enter y:"));
+    createCanva(x, y);
+})
+
+createCanva(x, y);
